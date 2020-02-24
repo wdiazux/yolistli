1d052259b7e566a4b9d63ffadca5b55f11ac4698
Aaron Reynoza <aaron.reynoza@modusbox.com>
2020-02-23 23:51:25 -0600

    change favicon to match Yolistli's

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,45 +1,42 @@
 module.exports = {
-  root: true,
-  parser: "@typescript-eslint/parser",
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:@typescript-eslint/recommended",
-    "prettier/@typescript-eslint",
-    "plugin:prettier/recommended",
-  ],
-  settings: {
-    react: {
-      version: "detect",
+    root: true,
+    parser: '@typescript-eslint/parser',
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:@typescript-eslint/recommended',
+        'prettier/@typescript-eslint',
+        'plugin:prettier/recommended',
+    ],
+    settings: {
+        react: {
+            version: 'detect',
+        },
     },
-  },
-  env: {
-    es6: true,
-    browser: true,
-    node: true,
-  },
-  plugins: [
-    "@typescript-eslint",
-    "react"
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        es6: true,
+        browser: true,
+        node: true,
     },
-    ecmaVersion: 6, // Allows for the parsing of modern ECMAScript features
-    sourceType: "module", // Allows for the use of imports
-  },
-  rules: {
-    "react/prop-types": "off", // Disable prop-types as we use TypeScript for type checking
-    "@typescript-eslint/explicit-function-return-type": "off",
-  },
-  overrides: [
-    // Override some TypeScript rules just for .js files
-    {
-      files: ["*.js"],
-      rules: {
-        "@typescript-eslint/no-var-requires": "off", //
-      },
+    plugins: ['@typescript-eslint', 'react'],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 6, // Allows for the parsing of modern ECMAScript features
+        sourceType: 'module', // Allows for the use of imports
     },
-  ],
+    rules: {
+        'react/prop-types': 'off', // Disable prop-types as we use TypeScript for type checking
+        '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+    overrides: [
+        // Override some TypeScript rules just for .js files
+        {
+            files: ['*.js'],
+            rules: {
+                '@typescript-eslint/no-var-requires': 'off', //
+            },
+        },
+    ],
 }
